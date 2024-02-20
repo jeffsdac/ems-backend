90777f77f37d5f4818065d35422f79e3ee37f1a1
Jefferson De Andrade <jeffersonandrade1999@gmail.com>
2024-02-20 20:51:30 -0300

    initial commit

---
+++ src/main/java/net/javaguides/emsbackend/DemoProjectForSpringBootEmployeeManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.emsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectForSpringBootEmployeeManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjectForSpringBootEmployeeManagmentSystemApplication.class, args);
+	}
+
+}
